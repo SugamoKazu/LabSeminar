3e19def3ba3d96721f124308c3782ee4439f4ed3
Sugamoto Kazuki <221205081@ccmailg.meijo-u.ac.jp>
2024-11-26 23:22:10 +0900

    手の動作（キー入力）

--- Assets/DevSugamoto/HandMove.cs
+++ Assets/DevSugamoto/HandMove.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class HandMove : MonoBehaviour
 {
+    private Transform myTransform;
+    private Vector3 position;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTransform = this.transform;
+        position = myTransform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey("up"))
+        {
+            position.z += 0.3f;
+        }
+        if(Input.GetKey("right"))
+        {
+            position.x += 0.3f;
+        }
+        if(Input.GetKey("down"))
+        {
+            position.z -= 0.3f;
+        }
+        if(Input.GetKey("left"))
+        {
+            position.x -= 0.3f;
+        }
+
+        myTransform.position = position;
         
     }
 }
--- Assets/DevSugamoto/MoleMove.cs
+++ Assets/DevSugamoto/MoleMove.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class MoleMove : MonoBehaviour
 {
+    private Transform myTransform;
+    private Vector3 position;
+    private int moveCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTransform = this.transform;
+        position = myTransform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveCount++;
+        var moveVec = 0.1f;
+
+        if(40 < moveCount && moveCount < 120) //停止時間を動的に？
+        {
+            moveVec *= 0;
+        }
+        else if(120 < moveCount)
+        {
+            moveVec *= -2;
+        }
+
+
+        Debug.Log(moveVec);
+        position.y += moveVec;
+
+        if(moveCount == 140) //難易度調整の余地あり
+        {
+            Destroy(this.gameObject);
+        }
+
+        myTransform.position = position;
+
+
     }
 }
