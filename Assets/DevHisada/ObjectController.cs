0fc26191f619bb65eb9f54d5fe14ff9285535064
Takumi Hisada <221205136@ccmailg.meijo-u.ac.jp>
2024-12-09 18:13:37 +0900

    ESP32との通信

---
+++ Assets/DevHisada/ObjectController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectController : MonoBehaviour
+{
+    public SerialHandler serialHandler;
+    public float delta = 0.01f;
+    private float deltaPos;
+    private    Vector3 pos;
+
+    void Start()
+    {
+        serialHandler.OnDataReceived += OnDataReceived;
+    }
+
+    void OnDataReceived(string message)
+    {
+        Vector3 pos = transform.localPosition;
+        var data = message.Split(
+            new string[]{"\n"}, System.StringSplitOptions.None);
+        Debug.Log(message);
+        
+        switch (data[0]){
+            case "Up":
+                deltaPos = delta * 1;
+                pos.y += deltaPos;
+                break;
+            case "Down":
+                deltaPos = delta * -1;
+                pos.y += deltaPos;
+                break;
+            case "Left":
+                deltaPos = delta * -1;
+                pos.x += deltaPos;
+                break;
+            case "Right":
+                deltaPos = delta * 1;
+                pos.x += deltaPos;
+                break;
+            default:
+                break;
+        }
+        transform.localPosition = pos;
+    }  
+}
